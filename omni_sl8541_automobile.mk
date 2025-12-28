4e5bfa670e78d721f87dede31028478fd1eb7d05
RikkaSakura <ricerica@outlook.com>
2025-12-28 16:07:25 +0800

    update

--- omni_sl8541_automobile.mk
+++ omni_sl8541_automobile.mk
@@ -8,9 +8,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from sl8541_automobile device
 $(call inherit-product, device/sprd/sl8541_automobile/device.mk)
 
